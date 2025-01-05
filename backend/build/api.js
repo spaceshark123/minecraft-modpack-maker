413351718643ca111dab07d4503d4122a4a8a4dc
spaceshark123 <amogh.patnaik@icloud.com>
2025-01-05 01:14:54 -0600

    refactor backend to serve frontend and backend separately

--- backend/build/api.js
+++ backend/build/api.js
@@ -4,14 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors = require('cors');
+const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-app.use(cors());
+const port = process.env.PORT || 3000;
+app.use((0, cors_1.default)());
+// serve the frontend
+app.use(express_1.default.static(path_1.default.join(__dirname, '..', '..', 'frontend', 'dist')));
 app.get('/', (req, res) => {
-    // enable cors
-    res.header('Access-Control-Allow-Origin', '*');
+    // display the frontend
+    res.sendFile(path_1.default.join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'));
+});
+app.get('/api', (req, res) => {
     res.send('Hello World!');
 });
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is running on http://localhost:3000');
 });
--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,14 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+// serve the frontend
+app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'dist')));
+
+app.get('/', (req, res) => {
+	// display the frontend
+	res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'));
+});
 
 app.get('/api', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(port, () => {
+	console.log('Server is running on http://localhost:3000');
 });
