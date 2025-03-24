3a95440e6b744a59ac6c4992d34fefe0d2f43d65
spaceshark123 <amogh.patnaik@icloud.com>
2025-03-24 14:49:35 -0500

    add unit tests for /api/file and /api/mod/modrinth endpoints

--- backend/test/api/api.test.ts
+++ backend/test/api/api.test.ts
@@ -22,3 +22,65 @@ describe('GET /api', () => {
 		expect(response.text).toBe('Hello World!');
 	});
 });
+
+describe('GET /api/file', () => {
+	let response: Response;
+
+	beforeAll(async () => {
+		response = await server.get('/api/file').query({ url: 'https://file-examples.com/storage/fe75a92b1367e14f59b6db4/2017/02/file_example_JSON_1kb.json' });
+	});
+
+	it('should return 200 OK', async () => {
+		expect(response.status).toBe(200);
+	});
+});
+
+describe('GET /api/mod/modrinth', () => {
+	let response: Response;
+
+	it('successfully fetches mod data from Modrinth', async () => {
+		response = await server.get('/api/mod/modrinth').query({
+			name: 'fabric api',
+			version: '1.20.1',
+			loader: 'fabric'
+		});
+
+		expect(response.status).toBe(200);
+		expect(response.body).toHaveProperty('title', 'Fabric API');
+		expect(response.body).toHaveProperty('slug', 'fabric-api');
+		expect(response.body).toHaveProperty('id', 'P7dR8mSH');
+		expect(response.body).toHaveProperty('image');
+		expect(response.body).toHaveProperty('similarity',1);
+	});
+
+	it('returns 404 for invalid mod', async () => {
+		response = await server.get('/api/mod/modrinth').query({
+			name: 'lorem ipsum',
+			version: '1.0.0',
+			loader: 'fabric'
+		});
+
+		expect(response.status).toBe(404);
+	});
+
+	it('returns 400 for missing parameters', async () => {
+		response = await server.get('/api/mod/modrinth');
+
+		expect(response.status).toBe(400);
+	});
+
+	it('fuzzy searches for similar mods', async () => {
+		response = await server.get('/api/mod/modrinth').query({
+			name: 'fabric a',
+			version: '1.20.1',
+			loader: 'fabric'
+		});
+
+		expect(response.status).toBe(200);
+		expect(response.body).toHaveProperty('title', 'Fabric API');
+		expect(response.body).toHaveProperty('slug', 'fabric-api');
+		expect(response.body).toHaveProperty('id', 'P7dR8mSH');
+		expect(response.body).toHaveProperty('image');
+		expect(response.body).toHaveProperty('similarity', 0.7200000000000001);
+	});
+});
